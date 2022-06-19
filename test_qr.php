766a4fb8b02ffb09e6e8f47c5883ad3ef818feb9
luggiestar <luggiestar@gmail.com>
2022-06-19 15:01:50 +0300

    password alter

--- test_qr.php
+++ test_qr.php
@@ -1,31 +1,9 @@
 <?php
-	$mysqli = new mysqli('localhost', 'root', '', 'e_ticketing'); 
-
-	$data = mysqli_query($mysqli, "SELECT * FROM tbl_bus");
-	echo "<table><tr>
-		<th>ID</th>
-		<th>Username</th>
-		<th>cAPACTY</th>
-		</tr>";
-	while($row = $data->fetch_assoc()){
-		$user_id = $row['id'];
-		$username = $row['plate_no'];
-		$password = $row['capacity'];
-		echo "<tr>
-		<td>$user_id</td>
-		<td>$username</td>
-		<td>$password</td>";
-	}
-	echo "</tr></table>";
-
-	// $phone = "255712506010";
-
-	// if(preg_match("/^255+[67]+[12345678]+[1-9]/", $phone)) {
-	// 	echo  "yes";
-	// }
-
-	// else {
-	// 	echo  "no";
-	// }
+//weke database connection
+$password = password_hash("1234", PASSWORD_DEFAULT);
+$sql = "UPDATE tbl_user SET password = :password WHERE id = 1";
+$stmt = $dbconnect->prepare($sql);
+$stmt->execute(['password'=>$password]);
  
 ?>
+
